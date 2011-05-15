a96b0ed5329bdaa865de25a88e37def89490e3a3
John Hudson <johnathon.hudson@gmail.com>
2011-05-15 17:04:47 +0930

    updated servlet

--- src/main/java/org/java/UploadServlet.java
+++ src/main/java/org/java/UploadServlet.java
@@ -1,10 +1,11 @@
 package org.java;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
-import java.util.Iterator;
-import java.util.List;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -12,80 +13,75 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.FileUploadException;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.IOUtils;
 
 public class UploadServlet extends HttpServlet {
-  private static final long serialVersionUID = 1L;
-  private static final String TMP_DIR_PATH = "/temp";
-  private File tmpDir;
-  private static final String DESTINATION_DIR_PATH = "/files";
-  private File destinationDir;
+  private static final long serialVersionUID = 6748857432950840322L;
+  private static final String DESTINATION_DIR_PATH = "files";
+  private static String realPath;
 
+  /**
+   * {@inheritDoc}
+   * 
+   * @param config
+   * @throws ServletException
+   */
+  @Override
   public void init(ServletConfig config) throws ServletException {
     super.init(config);
-    tmpDir = new File(TMP_DIR_PATH);
-    if (!tmpDir.isDirectory()) {
-      throw new ServletException(TMP_DIR_PATH + " is not a directory");
-    }
-    String realPath = getServletContext().getRealPath(DESTINATION_DIR_PATH);
-    destinationDir = new File(realPath);
-    if (!destinationDir.isDirectory()) {
-      throw new ServletException(DESTINATION_DIR_PATH + " is not a directory");
-    }
+    realPath = getServletContext().getRealPath(DESTINATION_DIR_PATH) + "/";
   }
 
+  /**
+   * Handles the HTTP <code>POST</code> method.
+   * 
+   * @param request
+   *          servlet request
+   * @param response
+   *          servlet response
+   * @throws ServletException
+   *           if a servlet-specific error occurs
+   * @throws IOException
+   *           if an I/O error occurs
+   */
+  @Override
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-    PrintWriter out = response.getWriter();
-    response.setContentType("text/plain");
-    out.println("<h1>Servlet File Upload Example using Commons File Upload</h1>");
-    out.println();
+      throws ServletException {
 
-    DiskFileItemFactory fileItemFactory = new DiskFileItemFactory();
-    /*
-     * Set the size threshold, above which content will be stored on disk.
-     */
-    fileItemFactory.setSizeThreshold(1 * 1024 * 1024); // 1 MB
-    /*
-     * Set the temporary directory to store the uploaded files of size above threshold.
-     */
-    fileItemFactory.setRepository(tmpDir);
+    PrintWriter writer = null;
+    InputStream is = null;
+    FileOutputStream fos = null;
 
-    ServletFileUpload uploadHandler = new ServletFileUpload(fileItemFactory);
     try {
-      /*
-       * Parse the request
-       */
-      List items = uploadHandler.parseRequest(request);
-      Iterator itr = items.iterator();
-      while (itr.hasNext()) {
-        FileItem item = (FileItem) itr.next();
-        /*
-         * Handle Form Fields.
-         */
-        if (item.isFormField()) {
-          out.println("File Name = " + item.getFieldName() + ", Value = " + item.getString());
-        } else {
-          // Handle Uploaded files.
-          out.println("Field Name = " + item.getFieldName() + ", File Name = " + item.getName()
-              + ", Content type = " + item.getContentType() + ", File Size = " + item.getSize());
-          /*
-           * Write file to the ultimate location.
-           */
-          File file = new File(destinationDir, item.getName());
-          item.write(file);
-        }
-        out.close();
-      }
-    } catch (FileUploadException ex) {
-      log("Error encountered while parsing the request", ex);
-    } catch (Exception ex) {
-      log("Error encountered while uploading file", ex);
+      writer = response.getWriter();
+    } catch (IOException ex) {
+      log(UploadServlet.class.getName() + "has thrown an exception: " + ex.getMessage());
     }
 
-  }
+    String filename = request.getHeader("X-File-Name");
+    try {
+      is = request.getInputStream();
+      fos = new FileOutputStream(new File(realPath + filename));
+      IOUtils.copy(is, fos);
+      response.setStatus(response.SC_OK);
+      writer.print("{success: true}");
+    } catch (FileNotFoundException ex) {
+      response.setStatus(response.SC_INTERNAL_SERVER_ERROR);
+      writer.print("{success: false}");
+      log(UploadServlet.class.getName() + "has thrown an exception: " + ex.getMessage());
+    } catch (IOException ex) {
+      response.setStatus(response.SC_INTERNAL_SERVER_ERROR);
+      writer.print("{success: false}");
+      log(UploadServlet.class.getName() + "has thrown an exception: " + ex.getMessage());
+    } finally {
+      try {
+        fos.close();
+        is.close();
+      } catch (IOException ignored) {
+      }
+    }
 
+    writer.flush();
+    writer.close();
+  }
 }
